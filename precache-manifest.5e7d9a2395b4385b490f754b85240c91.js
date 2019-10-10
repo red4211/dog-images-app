82f746fa40c5d57976a3ab395d16fdf8632e1b79
red42 <mperepelitsa421@gmail.com>
2019-10-10 17:49:49 +0300

    Updates

---
+++ precache-manifest.5e7d9a2395b4385b490f754b85240c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f96fadfcb0974040bc8f9eb11546a33",
+    "url": "/dog-images-app/index.html"
+  },
+  {
+    "revision": "ba1ab41026e05d944324",
+    "url": "/dog-images-app/static/css/main.2ab1821c.chunk.css"
+  },
+  {
+    "revision": "18c550a6e9a7d21d7e64",
+    "url": "/dog-images-app/static/js/2.9088ebdd.chunk.js"
+  },
+  {
+    "revision": "ba1ab41026e05d944324",
+    "url": "/dog-images-app/static/js/main.3127e6f4.chunk.js"
+  },
+  {
+    "revision": "4937d06eb41f3d7ae5f4",
+    "url": "/dog-images-app/static/js/runtime-main.58f3d538.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dog-images-app/static/media/logo.25bf045c.svg"
+  }
+]);
